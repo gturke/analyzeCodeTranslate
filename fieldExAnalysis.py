b9410d0491844e5cc8695e88a6cc4941a80a735e
Grace <grace.turke@gmail.com>
2016-08-03 16:21:23 -0400

    Updates fieldExAnalysis code with functions reformatConditions, logitProcess, and analyze. Works when analyzing tests with multiple treatment groups/where control is specified. TODO: fix so runs when only one treatment group, but data is not already coded 0/1.

--- fieldExAnalysis.py
+++ fieldExAnalysis.py
@@ -19,10 +19,9 @@ class fieldExAnalysis(object):
     dv - The dependent variable, generally a survey response or vote history, which should have been recoded as a dummy variable by the user prior to calling this function. This must be a string, which must be the name of a column in data frame universeDf.
     condition - The treatment assignment condition. # TODO - Write code to automatically recode condition to 0/1 dummies
     control
-    numOutputs - (optional) The number of outputs the user expects to exist in the dependent variable values. This is meant to be a useful check for the user to ensure what the expect matches the data the program is running on.
     covariates
     """
-    def __init__(self, universeDf, dv, condition, control=None, numConditions = None, numOutputs=None, covariates=None): #TODO: Should treatment groups be acceptable as categorical variables, and we recode to 0/1s? I think yes. ID which var is control.
+    def __init__(self, universeDf, dv, condition, control=None, numConditions = None, covariates=None): #TODO: Should treatment groups be acceptable as categorical variables, and we recode to 0/1s? I think yes. ID which var is control.
         self.universeDf = universeDf
         if not isinstance(self.universeDf, pd.DataFrame):
             raise Exception('Argument universeDf requires DataFrame object.')
@@ -41,12 +40,9 @@ class fieldExAnalysis(object):
         elif self.condition not in self.universeDf.columns:
             raise Exception('Column {} does not exist in your data frame.'.format(self.condition))
             
-        self.numOutputs = numOutputs # TODO: Use this one as a check with "proceed? y/n" option
         numOutputsObserved = len(set(self.universeDf[self.dv]))
-        if self.numOutputs == None:
-            self.numOutputs = numOutputsObserved
-        if numOutputsObserved != self.numOutputs:
-            raise Exception('Number of dependent variable outputs observed ({}) does not equal number of dependent variable outputs expected ({}). The number of dependent variable outputs should equal the number of treatment groups + 1 (for the control group).'.format(numOutputsObserved,self.numOutputs))
+        if numOutputsObserved != 2:
+            raise Exception('Number of dependent variable outputs observed ({}) does not equal 2. The dependent variable should be coded in numeric, dummy variable format so all values equal 0 or 1.'.format(numOutputsObserved))
         
         self.numConditions = numConditions # TODO: Use this one as a check with "proceed? y/n" option
         numConditionsObserved = len(set(self.universeDf[self.condition]))
@@ -54,12 +50,12 @@ class fieldExAnalysis(object):
             self.numConditions = numConditionsObserved
         if numConditionsObserved != self.numConditions:
             raise Exception('Number of conditions observed ({}) does not equal number of conditions expected ({})..'.format(numConditionsObserved,self.numConditions))
-        
-        
+
         self.covariates = covariates
-        if self.covariates != None:
-            if not isinstance(self.covariates,list):
-                raise Exception('Object "covariates" must be submitted as a list of strings. Default option is None. Use default if no covariate are to be used in analysis.')
+        if self.covariates == None:
+            self.covariates = []
+        if not isinstance(self.covariates,list):
+            raise Exception('Object "covariates" must be submitted as a list of strings. Default option is None. Use default if no covariate are to be used in analysis.')
         
         self.control = control
         if self.control == None:
@@ -72,27 +68,44 @@ class fieldExAnalysis(object):
         if self.control not in set(self.universeDf[self.condition]):
             raise Exception('Control condition "{}" does not appear in your data set.'.format(self.control))
 
-            
-    # Use this function if you want to evaluate the impact of multiple treatment assignments
     def reformatConditions(self):
         treatVars = list(set(self.universeDf[self.condition]))
         treatVars.remove(self.control)
-                
         n = 1
+        reformattedDf = self.universeDf
         for var in treatVars:
-            print ("Treatvar: {0}, n: {1}, treatvars list: {2}".format(var, n, treatVars))
-            self.universeDf['treat_{}'.format(n)] = None
-            # WRITE CODE HERE TO  REPLACE VALS
+            newColDf = pd.DataFrame(index=range(len(reformattedDf)),columns=['treat_{}'.format(n)],dtype= float)
+            reformattedDf = reformattedDf.join(newColDf)
             
+            dict = {1:0,n+1:1}
+            # TODO: ASK AUDREY - THERE HAS  TO BE A BETTER WAY TO DO THIS...
+            reformattedDf.loc[reformattedDf[self.condition].isin(dict.keys()),'treat_{}'.format(n)] = reformattedDf[self.condition].map(dict)
             n +=1
-            
-
+        return reformattedDf
     
-    #def analyzeLogit(self):
-
+    def logitProcess(self,df,y,x):
+        df['intercept'] = 1.0
+        logit = sm.Logit(df[y], df[[x,'intercept']+self.covariates])
+        result = logit.fit()
+        del df['intercept']
+        return result.summary()
+    
+    # Save data frame as object
+    # Display which condition treat_n corresponds to...? TODO: I think it would be better to rewrite so it just gives the value you started with as the column name
+        
+    def analyze(self):
+        lenCondition = len(set(self.universeDf[self.condition]))
+        if lenCondition >=3:
+            listConditions = list(range(lenCondition))
+            listConditions.remove(0)
+            analysisDf = self.reformatConditions()
+            for cond in listConditions:
+                treatDf = analysisDf[analysisDf['treat_{}'.format(cond)].notnull()].reset_index()
+                print(self.logitProcess(treatDf,self.dv,'treat_{}'.format(cond)))
+        else:
+            return self.logitProcess(self.universeDf,self.dv,self.condition)
+    
 
 # TODO:
-# Add code to deal with treatment conditions, ID control
-# If more than one condition, logit each treatment group separately compared to control
 # Write function to check for significant differences between treatment groups
 # Code to create visuals: bar charts, tables; or nicely spit out numbers that can be quickly be dropped in a Microsoft PPT/Excel
